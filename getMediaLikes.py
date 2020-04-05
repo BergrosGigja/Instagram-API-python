b5a380e63d993a436dcf38f97b0cab3054cbb60f
Bergrós Gígja <bergros.gigja@gmail.com>
2020-04-05 20:10:07 +0000

    gets upto 18 photos and all likes and lists them with top 10 likers

--- getMediaLikes.py
+++ getMediaLikes.py
@@ -1,13 +1,13 @@
 from InstagramAPI import InstagramAPI
 from collections import Counter
 import argparse
-
-likes_list = []
+import sys
 
 # parse cmd line args
 parser = argparse.ArgumentParser(description='Unfollow instagram users that don\'t follow you back!.')
 parser.add_argument('username', help='your instagram username')
 parser.add_argument('password', help='your instagram password')
+parser.add_argument('usernameToTest', help='instagram username to test with')
 args = parser.parse_args()
 
 ig = InstagramAPI(args.username, args.password)
@@ -18,18 +18,29 @@ if not success:
     print('INSTAGRAM LOGIN FAILED!')
     sys.exit()
 
-ig.searchUsername(args.username) #Gets most recent post from user
+#Gets most recent post from user
+ig.searchUsername(args.usernameToTest)
 info = ig.LastJson
 username_id = info['user']['pk']
+
+likes_list = []
+
 user_posts = ig.getUserFeed(username_id)
 info = ig.LastJson
 
-for i in range(15):
+imageCount = len(info['items'])
+
+for i in range(imageCount):
     media_id = info['items'][i]['id']
-    ig.getMediaLikers(media_id)
+    likes = ig.getMediaLikers(media_id)
+    if not success:
+        sys.exit()
+
     f = ig.LastJson['users']
     for x in f:
         likes_list.append(x['username'])
 
 counted = Counter(likes_list)
-print(counted.most_common(5))
+print("Top 10 likers out of ", imageCount, " images for user", args.usernameToTest, ":")
+for x in counted.most_common(10):
+        print(x)
